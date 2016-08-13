db53362cb29e8400c9803ad9e5b47e2ddc493fe7
Nate Williams <nate@yogotech.com>
2016-08-13 10:18:52 -0600

    Spinup multiples Plants

--- Plant.java
+++ Plant.java
@@ -2,22 +2,37 @@ public class Plant implements Runnable {
     // How long do we want to run the juice processing
     public static final long PROCESSING_TIME = 5 * 1000;
 
+    private static final int NUM_PLANTS = 3;
+
     public static void main(String[] args) {
-        // Startup a single plant
-        Plant p = new Plant(1);
-        p.startPlant();
+        // Startup the plants
+        Plant[] plants = new Plant[NUM_PLANTS];
+        for (int i = 0; i < NUM_PLANTS; i++) {
+           plants[i] = new Plant(i + 1);
+           plants[i].startPlant();
+        }
 
         // Give the plants time to do work
         delay(PROCESSING_TIME, "Plant malfunction");
 
         // Stop the plant, and wait for it to shutdown
-        p.stopPlant();
-        p.waitToStop();
+        for (Plant p : plants) {
+           p.stopPlant();
+           p.waitToStop();
+        }
 
         // Summarize the results
-        System.out.println("Total processed = " + p.getOranges());
-        System.out.println("Created " + p.getBottles() +
-                           ", wasted " + p.getWaste() + " oranges");
+        int oranges = 0;
+        int bottles = 0;
+        int waste = 0;
+        for (Plant p : plants) {
+            oranges += p.getOranges();
+            bottles += p.getBottles();
+            waste += p.getWaste();
+        }
+        System.out.println("Total processed = " + oranges);
+        System.out.println("Created " + bottles +
+                           ", wasted " + waste + " oranges");
     }
 
     private static void delay(long time, String errMsg) {
